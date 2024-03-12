316d258618af0fe1d6f2955c2df49c6d513160ca
ADNAN-an <adnan.anjar2002@gmail.com>
2024-03-12 15:01:54 +0000

    Add project files.

---
+++ EcoWatch/Models/BlogPost.cs
@@ -0,0 +1,11 @@
+﻿namespace EcoWatch.Models
+{
+    public class BlogPost
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        
+     }
+}
---
+++ EcoWatch/Models/Report.cs
@@ -0,0 +1,14 @@
+﻿namespace EcoWatch.Models
+{
+    public class Report
+    {
+        public int ReportId { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; }
+        public string Type { get; set; }
+        public byte[] Image { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
