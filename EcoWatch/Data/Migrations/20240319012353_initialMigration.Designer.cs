d0a1240b0653c14f66cd5f64923325042e016209
ADNAN-an <adnan.anjar2002@gmail.com>
2024-03-19 02:25:25 +0000

    init

--- EcoWatch/Data/Migrations/20240319012353_initialMigration.Designer.cs
+++ EcoWatch/Data/Migrations/20240319012353_initialMigration.Designer.cs
@@ -4,6 +4,7 @@ using EcoWatch.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EcoWatch.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240319012353_initialMigration")]
+    partial class initialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EcoWatch/Models/BlogPost.cs
+++ EcoWatch/Models/BlogPost.cs
@@ -1,9 +1,18 @@
-﻿namespace EcoWatch.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcoWatch.Models
 {
     public class BlogPost
     {
+        [Key]
         public int PostId { get; set; }
+
+        [ForeignKey("UserId")]
         public int UserId { get; set; }
+         
+        public IdentityUser User { get; set; }= new IdentityUser(); 
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
         
